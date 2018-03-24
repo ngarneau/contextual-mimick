39d9c7fb6c0f0b213e62287c531f6542bce830ae
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2018-03-23 20:42:18 -0400

    Add cosine distance in eval

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,7 @@ import numpy
 import logging
 
 from utils import load_embeddings
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def evaluate_embeddings(embeddings):
@@ -13,21 +14,25 @@ def evaluate_embeddings(embeddings):
     # Compare distance of mimick with the glove embeddings
     logging.info('Comparing the distance of Pinter\'s Mimick implementation...')
     mimick_distances = list()
+    mimick_cosine_distances = list()
     mimick_embeddings = load_embeddings('./embeddings/previous_mimick_validation_embeddings.txt')
     for word, embedding in mimick_embeddings.items():
         if word in glove_embeddings:
             target_embedding = glove_embeddings[word]
             mimick_distances.append(numpy.linalg.norm(embedding - target_embedding))
-    logging.info("Mimick distance: {}, {} ({})".format(numpy.mean(mimick_distances), numpy.std(mimick_distances), len(mimick_distances)))
+            mimick_cosine_distances.append(cosine_similarity(embedding.reshape(1, -1), target_embedding.reshape(1, -1)))
+    logging.info("Mimick distance: {}, var: {}, cosine: {}, ({})".format(numpy.mean(mimick_distances), numpy.std(mimick_distances), numpy.mean(mimick_cosine_distances), len(mimick_distances)))
 
     # Compare distance of our implementation with the glove embeddings
     logging.info('Comparing the distance of our embeddings with GloVe embeddings...')
     our_distances = list()
+    our_cosine_distances = list()
     for word, embedding in embeddings.items():
         if word in glove_embeddings:
             target_embedding = glove_embeddings[word]
             our_distances.append(numpy.linalg.norm(embedding - target_embedding))
-    logging.info("Our distance: {}, {} ({})".format(numpy.mean(our_distances), numpy.std(our_distances), len(our_distances)))
+            our_cosine_distances.append(cosine_similarity(embedding.reshape(1, -1), target_embedding.reshape(1, -1)))
+    logging.info("Our distance: {}, var: {}, cosine: {}, ({})".format(numpy.mean(our_distances), numpy.std(our_distances), numpy.mean(our_cosine_distances), len(our_distances)))
 
     # Compare distance of our embeds from mimick's
     logging.info('Comparing the distance of our embeddings with Yuval\'s Mimick...')
