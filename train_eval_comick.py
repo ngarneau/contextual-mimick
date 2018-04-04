e6bf33f85fef7788531eed9d070d1af7f1d799b6
jsleb333 <jean-samuel.leboeuf@hotmail.com>
2018-04-04 18:44:21 -0400

    added debug mode, verbose and evaluation func

--- train_eval_comick.py
+++ train_eval_comick.py
@@ -5,18 +5,20 @@ logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
 from comick import Comick, LRComick
-from utils import load_embeddings, parse_conll_file, cosine_sim, cosine_distance
-from utils import square_distance
+from utils import load_embeddings, save_embeddings, parse_conll_file
+from utils import square_distance, euclidean_distance, cosine_sim, cosine_distance
 from utils import make_vocab, WordsInContextVectorizer, ngrams
 from utils import collate_fn
 from per_class_dataset import *
 
-import numpy
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from pytoune import torch_to_numpy, tensors_to_variables
 from pytoune.framework import Model
 from pytoune.framework.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, CSVLogger
 from torch.optim import Adam
 
-def load_data(d):
+def load_data(d, verbose=True, debug_mode=False):
     path_embeddings = './embeddings_settings/setting1/1_glove_embeddings/glove.6B.{}d.txt'.format(d)
     try:
         train_embeddings = load_embeddings(path_embeddings)
@@ -24,14 +26,20 @@ def load_data(d):
         if d == 50:
             path_embeddings = './embeddings/train_embeddings.txt'
             train_embeddings = load_embeddings(path_embeddings)
-            print('Loading {}d embeddings from: "' + path_embeddings + '"'.format(d))
         else:
             raise
-    print('Loading ' + str(d) + 'd embeddings from: "' + path_embeddings + '"')
 
     train_sentences = parse_conll_file('./conll/train.txt')
     valid_sentences = parse_conll_file('./conll/valid.txt')
     test_sentences = parse_conll_file('./conll/test.txt')
+
+    if verbose:
+        print('Loading ' + str(d) + 'd embeddings from: "' + path_embeddings + '"')
+
+    if debug_mode:
+        train_sentences = train_sentences[:100]
+        valid_sentences = valid_sentences[:100]
+        test_sentences = test_sentences[:100]
 
     return train_embeddings, (train_sentences, valid_sentences, test_sentences)
 
@@ -91,10 +99,10 @@ def prepare_data(embeddings,
     return train_loader, valid_loader
 
 
-def train(model_name, char_to_idx, word_to_idx, train_embeddings, d, use_gpu=False, freeze_word_embeddings=False):
+def train(model_name, char_to_idx, word_to_idx, train_embeddings, d, train_loader, valid_loader, use_gpu=False, freeze_word_embeddings=False, debug_mode=False):
     
     # Create the model
-    net = Comick(characters_vocabulary=char_to_ix,
+    net = Comick(characters_vocabulary=char_to_idx,
                  words_vocabulary=word_to_idx,
                  characters_embedding_dimension=20,
                  characters_hidden_state_dimension=50,
@@ -110,6 +118,10 @@ def train(model_name, char_to_idx, word_to_idx, train_embeddings, d, use_gpu=Fal
 
     if use_gpu:
         model.cuda()
+    if debug_mode:
+        epochs = 1
+    else:
+        epochs = 1000
 
     # Create callbacks and checkpoints
     lrscheduler = ReduceLROnPlateau(patience=2)
@@ -132,29 +144,82 @@ def train(model_name, char_to_idx, word_to_idx, train_embeddings, d, use_gpu=Fal
     callbacks = [lrscheduler, ckpt_best, ckpt_last, early_stopping, csv_logger]
 
     # Fit the model
-    model.fit_generator(train_loader, valid_loader, epochs=1000, callbacks=callbacks)
+    model.fit_generator(train_loader, valid_loader, epochs=epochs, callbacks=callbacks)
 
     return model
 
-def evaluate():
-    pass
+
+def predict(model, generator):
+    model.model.eval()
+    pred = []
+    for x, y in generator:
+        x = tensors_to_variables(x)
+        pred.append(torch_to_numpy(model.model(x)))
+    return np.concatenate(pred)
 
 
+def evaluate(model, test_sentences, train_sentences, test_embeddings, vectorizer, n=15, use_gpu=False, save=True, model_name=None):
+    
+    train_labels = set(token for sentence in train_sentences for token in sentence)
+    test_examples = set((ngram, ngram[1]) for sentence in test_sentences for ngram in ngrams(sentence, n) if ngram[1] not in train_labels)
+
+    test_dataset = PerClassDataset(dataset=test_examples,
+                                   target_transform=lambda y: np.array([0]*50),
+                                   transform=vectorizer)
+
+    loader = PerClassLoader(dataset=test_dataset,
+                            collate_fn=collate_fn,
+                            k=-1,
+                            batch_size=128,
+                            use_gpu=use_gpu)
+    
+    predicted_embeddings = predict(model, loader)
+    mean_pred_embeddings = {}
+    i = 0
+    for label, N in test_dataset:
+        if N > 0:
+            # mean_pred_embeddings[label] = [predicted_embeddings[i+j] for j in range(N)]
+            mean_pred_embeddings[label] = np.mean(predicted_embeddings[i:i+N], axis=0)
+            i += N
+    
+    if save:
+        if model_name == None: raise ValueError('A filename should be provided.')
+        save_embeddings(mean_pred_embeddings, model_name)
+
+    predicted_results = {}
+    norm = lambda y_true, y_pred: np.linalg.norm(y_pred.reshape(1,-1)-y_true)
+    sum_norm = 0
+    cos_sim = lambda y_true, y_pred: float(cosine_similarity(y_pred.reshape(1, -1), y_true))
+    sum_cos_sim = 0
+    i = 0
+    for label, y_pred in mean_pred_embeddings.items():
+        if label in test_embeddings:
+            y_true = test_embeddings[label].reshape(1,-1)
+            sum_norm += norm(y_true, y_pred)
+            sum_cos_sim += cos_sim(y_true, y_pred)
+            i += 1
+    
+    print('mean euclidean dist:', sum_norm/i, 'mean cosine sim:', sum_cos_sim/i)
+
+            
 def main(n=41, k=1, device=0, d=50):
-
     seed = 299792458  # "Seed" of light
     torch.manual_seed(seed)
-    numpy.random.seed(seed)
+    np.random.seed(seed)
     random.seed(seed)
 
+    debug_mode = False
+    verbose = True
+    save = True
     use_gpu = torch.cuda.is_available()
+    # use_gpu = False
     if use_gpu:
         cuda_device = device
         torch.cuda.set_device(cuda_device)
         print('Using GPU')
 
     # Load data
-    train_embeddings, sentences = load_data(d)
+    train_embeddings, sentences = load_data(d, verbose, debug_mode)
     train_sentences, valid_sentences, test_sentences = sentences
     all_sentences = sentences[0] + sentences[1] + sentences[2]
 
@@ -162,22 +227,43 @@ def main(n=41, k=1, device=0, d=50):
 
     # Prepare our examples
     vectorizer = WordsInContextVectorizer(word_to_idx, char_to_idx)
+    vectorizer = vectorizer.vectorize_unknown_example_merged_context
     train_loader, valid_loader = prepare_data(
         embeddings=train_embeddings,
         sentences=train_sentences,
-        vectorizer=vectorizer.vectorize_unknown_example_merged_context,
+        vectorizer=vectorizer,
         n=n,
         use_gpu=use_gpu,
         k=k,
-        over_population_threshold=80
+        over_population_threshold=80,
+        verbose=verbose,
     )
     
     # Create and train the model
     model_name = 'comick_n{}_k{}_d{}_unique_context'.format(n, k, d)
-    train(model_name, char_to_idx, word_to_idx, train_embeddings,
-          d=d,
-          use_gpu=use_gpu,
-          freeze_word_embeddings=False)
+    if debug_mode:
+        model_name = 'testing_' + model_name
+        save = False
+    model = train(
+        model_name, char_to_idx, word_to_idx, train_embeddings,
+        train_loader=train_loader, valid_loader=valid_loader,
+        d=d,
+        use_gpu=use_gpu,
+        debug_mode=debug_mode,
+        freeze_word_embeddings=False,
+    )
+    
+    evaluate(
+        model,
+        test_sentences=valid_sentences,
+        train_sentences=train_sentences,
+        test_embeddings=train_embeddings,
+        vectorizer=vectorizer,
+        n=n,
+        use_gpu=use_gpu,
+        save=save,
+        model_name=model_name + '.txt'
+    )
 
 
 if __name__ == '__main__':
@@ -189,7 +275,7 @@ if __name__ == '__main__':
         parser.add_argument("n", default=7, nargs='?')
         parser.add_argument("k", default=1, nargs='?')
         parser.add_argument("device", default=0, nargs='?')
-        parser.add_argument("d", default=100, nargs='?')
+        parser.add_argument("d", default=50, nargs='?')
         args = parser.parse_args()
         n = int(args.n)
         k = int(args.k)
