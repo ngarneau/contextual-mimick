b718048a19c7def15c726f25ba2965a8bb0cc904
jsleb333 <jean-samuel.leboeuf@hotmail.com>
2018-05-07 12:05:12 -0400

    relative over pop threshold

--- train_eval_comick.py
+++ train_eval_comick.py
@@ -8,10 +8,11 @@ logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
 from comick import ComickDev
-from utils import load_embeddings, save_embeddings, parse_conll_file, preprocess_token
+from utils import save_embeddings
 from utils import square_distance, cosine_sim
-from utils import make_vocab, WordsInContextVectorizer, ngrams
+from utils import make_vocab, WordsInContextVectorizer
 from utils import collate_fn, collate_x
+from data_preparation import *
 from per_class_dataset import *
 from downstream_task.part_of_speech.train import train as train_pos
 from downstream_task.named_entity_recognition.train import train as train_ner
@@ -27,150 +28,6 @@ from pytoune import torch_to_numpy, tensors_to_variables
 from pytoune.framework import Model
 from pytoune.framework.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, CSVLogger
 from torch.optim import Adam
-
-
-def load_data(d, corpus, verbose=True):
-    path_embeddings = './data/conll_embeddings_settings/setting1/glove/train/glove.6B.{}d.txt'.format(d)
-    embeddings = load_embeddings(path_embeddings)
-
-    train_sentences = parse_conll_file('./data/conll/train.txt')
-    valid_sentences = parse_conll_file('./data/conll/valid.txt')
-    test_sentences = parse_conll_file('./data/conll/test.txt')
-
-    if verbose:
-        logging.info('Loading ' + str(d) + 'd embeddings from: "' + path_embeddings + '"')
-
-    return embeddings, (train_sentences, valid_sentences, test_sentences)
-
-
-def augment_data(examples, embeddings):
-    labels = sorted(set(label for x, label in examples))
-    similar_words = pkl.load(open('./data/similar_words.p', 'rb'))
-
-    new_examples = dict()
-    for (left_context, word, right_context), label in examples:
-        if label in similar_words:
-            sim_words = similar_words[label]
-            for sim_word, cos_sim in sim_words:
-                # Add new labels, not new examples to already existing labels.
-                if sim_word not in labels and cos_sim >= 0.6:
-                    new_example = ((left_context, sim_word, right_context), sim_word)
-                    new_examples[new_example] = 1
-    return new_examples
-
-
-def preprocess_examples(examples):
-    preprocessed_examples = list()
-    for (left_context, word, right_context), label in examples:
-        preprocessed_word = preprocess_token(word)
-        preprocessed_examples.append((
-            (left_context, preprocessed_word, right_context),
-            label
-        ))
-    return preprocessed_examples
-
-
-def prepare_data(embeddings,
-                 test_vocabs,
-                 train_sentences,
-                 test_sentences,
-                 vectorizer,
-                 n=15,
-                 ratio=.8,
-                 use_gpu=False,
-                 k=1,
-                 over_population_threshold=None,
-                 verbose=True,
-                 data_augmentation=False):
-    # Train-validation part
-    examples = dict()
-    examples_without_embeds = dict()
-    for sentence in train_sentences:
-        for ngram in ngrams(sentence, n):
-            key = (ngram, ngram[1])
-            if ngram[1] in embeddings:
-                # Keeps only different ngrams which have a training embeddings
-                examples[key] = 1
-            else:
-                examples_without_embeds[key] = 1
-
-    if data_augmentation:
-        augmented_examples = augment_data(examples, embeddings)
-        if verbose:
-            logging.info("Number of non-augmented examples: {}".format(len(examples)))
-        examples.update(augmented_examples)  # Union
-    examples = preprocess_examples(examples)
-
-    transform = vectorizer.vectorize_unknown_example
-
-    def target_transform(y):
-        return embeddings[y]
-
-    train_valid_dataset = PerClassDataset(
-        examples,
-        transform=transform,
-        target_transform=target_transform,
-    )
-
-    train_dataset, valid_dataset = train_valid_dataset.split(
-        ratio=.8, shuffle=True, reuse_label_mappings=False)
-
-    filter_labels_cond = None
-    if over_population_threshold != None:
-        def filter_labels_cond(label, N): return N <= over_population_threshold
-    train_loader = PerClassLoader(dataset=train_dataset,
-                                  collate_fn=collate_fn,
-                                  batch_size=64,
-                                  k=k,
-                                  use_gpu=use_gpu,
-                                  filter_labels_cond=filter_labels_cond)
-    valid_loader = PerClassLoader(dataset=valid_dataset,
-                                  collate_fn=collate_fn,
-                                  batch_size=64,
-                                  k=k,
-                                  use_gpu=use_gpu,
-                                  filter_labels_cond=filter_labels_cond)
-
-    # Test part
-    test_examples = set((ngram, ngram[1]) for sentence in test_sentences for ngram in ngrams(sentence, n) if
-                        ngram[1] in test_vocabs)
-    test_examples = preprocess_examples(test_examples)
-
-    test_dataset = PerClassDataset(dataset=test_examples,
-                                   transform=transform)
-    test_loader = PerClassLoader(dataset=test_dataset,
-                                 collate_fn=collate_x,
-                                 k=-1,
-                                 batch_size=64,
-                                 use_gpu=use_gpu)
-
-    if verbose:
-        logging.info('Number of unique examples: {}'.format(len(examples)))
-        logging.info('Number of unique examples wo embeds:'.format(len(examples_without_embeds)))
-
-        logging.info('\nGlobal statistics:')
-        stats = train_valid_dataset.stats()
-        for stats, value in stats.items():
-            logging.info(stats + ': ' + str(value))
-
-        logging.info('\nStatistics on the training dataset:')
-        stats = train_dataset.stats(over_population_threshold)
-        for stats, value in stats.items():
-            logging.info(stats + ': ' + str(value))
-
-        logging.info('\nStatistics on the validation dataset:')
-        stats = valid_dataset.stats(over_population_threshold)
-        for stats, value in stats.items():
-            logging.info(stats + ': ' + str(value))
-
-        logging.info('\nStatistics on the test dataset:')
-        stats = test_dataset.stats()
-        for stats, value in stats.items():
-            logging.info(stats + ': ' + str(value))
-
-        logging.info('\nFor training, loading ' + str(k) + ' examples per label per epoch.')
-
-    return train_loader, valid_loader, test_loader
 
 
 def train(model, model_name, train_loader, valid_loader, epochs=1000):
@@ -268,7 +125,8 @@ def main(model_name, task_config, n=41, k=1, device=0, d=100, epochs=100):
     verbose = True
     save = True
     freeze_word_embeddings = True
-    over_population_threshold = 80
+    over_population_threshold = 100
+    relative_over_population = True
     data_augmentation = True
     if debug_mode:
         data_augmentation = False
@@ -279,10 +137,11 @@ def main(model_name, task_config, n=41, k=1, device=0, d=100, epochs=100):
     logging.info("Verbose: {}".format(verbose))
     logging.info("Freeze word embeddings: {}".format(freeze_word_embeddings))
     logging.info("Over population threshold: {}".format(over_population_threshold))
+    logging.info("Relative over population: {}".format(relative_over_population))
     logging.info("Data augmentation: {}".format(data_augmentation))
 
     use_gpu = torch.cuda.is_available()
-    # use_gpu = False
+    use_gpu = False
     if use_gpu:
         cuda_device = device
         torch.cuda.set_device(cuda_device)
@@ -304,7 +163,6 @@ def main(model_name, task_config, n=41, k=1, device=0, d=100, epochs=100):
     vectorizer = vectorizer
 
     # Prepare examples
-
     train_loader, valid_loader, test_loader = prepare_data(
         embeddings=embeddings,
         test_vocabs=test_vocabs,
@@ -315,6 +173,7 @@ def main(model_name, task_config, n=41, k=1, device=0, d=100, epochs=100):
         use_gpu=use_gpu,
         k=k,
         over_population_threshold=over_population_threshold,
+        relative_over_population=relative_over_population,
         data_augmentation=data_augmentation,
         verbose=verbose,
     )
