561f552bec4ab042700a6b8f551d11256f21ea5e
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2018-05-14 12:51:04 -0400

    Fix master branch

--- train_eval_comick.py
+++ train_eval_comick.py
@@ -7,7 +7,7 @@ from data_loaders import CoNLLDataLoader, SentimentDataLoader, SemEvalDataLoader
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
-from comick import Comick, ComickDev, ComickUniqueContext, LRComick
+from comick import ComickDev, ComickUniqueContext, LRComick
 from utils import save_embeddings
 from utils import square_distance, cosine_sim
 from utils import make_vocab, WordsInContextVectorizer
